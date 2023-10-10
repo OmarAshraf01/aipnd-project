ee0390a140b209d575aecb19263bc1a75ae45580
OmarAshraf01 <109757947+OmarAshraf01@users.noreply.github.com>
2023-10-11 00:57:16 +0300

    fixing issue in predict.py & classifier

--- predict.py
+++ predict.py
@@ -19,8 +19,8 @@ import time
 def arg_parse():
     parser = argparse.ArgumentParser(description="parser for predict.py")
     
-    parser.add_argument("img_path", type=str, help="Testing image path")
-    parser.add_argument("checkpoint", type=str,default='checkpoint_2.pth', help="Saved trained model checkpoint")
+    parser.add_argument("--img_path", type=str, help="Testing image path")
+    parser.add_argument("--checkpoint", type=str,default='checkpoint_2.pth', help="Saved trained model checkpoint")
     parser.add_argument("--top_k", type=int, default=5, help="Top K=")
     parser.add_argument("--category_names", type=str, default="cat_to_name.json", help="Json file to change folder labels to names")
     parser.add_argument("--device", type=str, default="cuda", choices=["cuda", "cpu"], help="Device to train on")
